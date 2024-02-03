784906810e45a269a131f61d575b961b94daba60
benjamin.crouch.ii <benjamin.crouch.ii@bwcii.com>
2024-02-03 15:44:02 +0000

    Added react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bwcii.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), react()],
   output: "server",
   adapter: node({
     mode: "standalone"
